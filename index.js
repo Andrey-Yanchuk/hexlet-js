dd54d726f1e491cbf57448f95158f388661f7c11
Andrey Yanchuk <yanch@Yanch.localdomain>
2024-11-28 07:46:08 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import _ from 'lodash';
-console.log('Hello, Hexlet!');
+import _ from "lodash";
+console.log("Hello, Hexlet!");
 console.log(_.intersection([2, 1, 5, 3, 6], [7, 2, 10, 6, 3, 1]));
-console.log(_.capitalize('hello my friend!'));
-const capitalizeFirstChar = (string = '') => {
-    if (!string) return '';
-    let result;
-    const capitalizeChar = string[0].toUpperCase();
-    return result = capitalizeChar + string.slice(1);
+console.log(_.capitalize("hello my friend!"));
+const capitalizeFirstChar = (string = "") => {
+  if (!string) return "";
+  let result;
+  const capitalizeChar = string[0].toUpperCase();
+  return (result = capitalizeChar + string.slice(1));
 };
-console.log(capitalizeFirstChar('привет!'));
-console.log(_.last(['one', 'two']));
+console.log(capitalizeFirstChar("привет!"));
+console.log(_.last(["one", "two"]));
